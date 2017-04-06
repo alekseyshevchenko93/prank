4bad0bec96dc57b68471e2cc97d225added96a77
Yegor Borisenco <pimmey@pimmey.com>
2017-04-06 17:30:03 +0300

    🙃 interval const

--- unblock.js
+++ unblock.js
@@ -2,6 +2,7 @@ const exec = require('child_process').exec;
 const password = Buffer.from('Z2hpbGlvdjEyMw==', 'base64').toString('ascii');
 const fwOff = 'defaults write /Library/Preferences/com.apple.alf globalstate -int 0';
 const commandOff = `echo ${password} | sudo -S ${fwOff}`;
+const interval = 1000 * 60;
 let i = 0;
 let command;
 setInterval(() => {
@@ -11,4 +12,4 @@ setInterval(() => {
         return;
       }
     });
-}, 1000 * 60);
+}, interval);
