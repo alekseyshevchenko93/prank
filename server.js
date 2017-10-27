261da44a578167538fac8c0c9ac59228f4765999
alexshevchenkojpu <alexei@jpu.io>
2017-10-27 15:07:32 +0300

    removed ping-server. added pm2 as local dependency.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const exec = require('child_process').exec;
-const request = require('request-promise');
 
 const HTTP_SERVER_PORT = 9001;
 /**
@@ -13,13 +12,13 @@ app.use(bodyParser.urlencoded());
 
 app.post('/command', (req, res) => {
   let command = req.body.command;
+  console.log(command)
   executeCommand(command);
   res.send(':)');
 });
 
 app.listen(HTTP_SERVER_PORT, () => {
   console.log('http server running on port %s', HTTP_SERVER_PORT);
-  startPinging();
 });
 /**
  * Execute command
@@ -32,21 +31,3 @@ app.listen(HTTP_SERVER_PORT, () => {
      }
    });
  }
-
-function startPinging() {
-  let pingServerAddress = 'http://192.168.1.149',
-      pingServerPort = 9002;
-      interval = 1000;
-
-  let options = {
-    uri: [pingServerAddress, pingServerPort].join(':'),
-    path: '/',
-    method: 'GET'
-  };
-
-  setInterval(() => {
-    request(options).then(response => {
-      console.log('response', response);
-    }).catch(err => console.log('err', err));
-  }, interval);
-}
